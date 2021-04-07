bb74279cf676c40a48bd636b604b51d60cfcf474
Lucheng <Lucheng@wondertek.com.cn>
2021-04-07 15:20:03 +0800

    菜单选项为函数式

---
+++ src/layout/item.vue
@@ -0,0 +1,36 @@
+<script>
+
+export default {
+  name: "MenuItem",
+  //设置为函数化组件
+	functional: true,
+  props: {
+    icon:{
+      type: String,
+      default:'',
+    },
+    title:{
+      type:String,
+      default:'',
+    }
+  },
+  render(h,context){
+    const {icon, title} = context.props
+    const vnodes = []
+    if(icon){
+      vnodes.push(<icon-svg icon-class={icon}/>)
+    }
+    if(title){
+      vnodes.push(<span slot='title' style="margin-left: 10px">{(title)}</span>)
+    }
+    return vnodes
+  }
+}
+</script>
+<style scoped lang="scss">
+  i {
+    margin-right: 10px;
+    color: #fff import !important;
+    font-weight: 700;
+  }
+</style>
---
+++ src/layout/sideItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <fragment v-if="!item.hidden" class="menu-wrapper">
+    <template
+       v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow"
+    >
+      <app-link>
+        <el-menu-item>
+          
+        </el-menu-item>
+      </app-link>
+    </template>
+    <el-submenu v-else red="subMenu" :index="resolvePath(item.path)" popper-append-to-body>
+      <template slot="title">
+        <item v-if="item.meta" :icon="item.meta && item.meta.icon" :title="item.meta.title"></item>
+      </template>
+    </el-submenu>
+  </fragment>
+</template>
+
+<script>
+import path from 'path'
+import Item from './item'
+export default {
+  //import引入的组件需要注入到对象中才能使用
+  components: {Item},
+  data() {
+    //这里存放数据
+    return {};
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {},
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+<style scoped></style>
