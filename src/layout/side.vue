03a5e8404c5eb4b4df2a01200a7c72bb72820dcf
Lucheng <Lucheng@wondertek.com.cn>
2021-04-07 11:18:58 +0800

    实现左侧导航折叠功能

--- src/layout/side.vue
+++ src/layout/side.vue
@@ -70,15 +70,20 @@ export default {
   //import引入的组件需要注入到对象中才能使用
   components: {},
   data() {
-    //这里存放数据
-    return {};
+    return {
+      collapse: false,
+    };
   },
   //监听属性 类似于data概念
   computed: {},
   //监控data中的数据变化
   watch: {},
   //方法集合
-  methods: {},
+  methods: {
+    collapseChange(){
+       this.collapse = !this.collapse;
+    }
+  },
   //生命周期 - 创建完成（可以访问当前this实例）
   created() {},
   //生命周期 - 挂载完成（可以访问DOM元素）
